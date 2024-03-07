1845d9805efe733af75073ecf886f64910c41c48
Dylan D <dndu@ualberta.ca>
2024-03-06 17:27:00 -0700

    implement deleteCity()

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -1,6 +1,6 @@
 package com.example.lab8;
 
-public class City {
+public class City implements Comparable{
 
     private String city;
     private String province;
@@ -8,6 +8,18 @@ public class City {
     public City(String city, String province){
         this.city = city;
         this.province = province;
+    }
+
+    /**
+     * This function overrides the comparison operator of the city function
+     * and now uses the city names for comparison
+     * @param o
+     * @return
+     */
+    @Override
+    public int compareTo(Object o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName()); // uses the Sting object compareTo
     }
 
     String getCityName(){
--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -57,6 +57,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public void deleteCity(City city){
+        cities.remove(city);
     }
 
 
