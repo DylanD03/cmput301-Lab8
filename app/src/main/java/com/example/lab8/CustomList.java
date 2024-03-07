7ca96da90a6147e4d4942e77b3816e99515d0d8e
Dylan D <dndu@ualberta.ca>
2024-03-06 17:19:17 -0700

    implement addCity()

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -50,7 +50,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public void addCity(City city){
-        //cities.add(city);
+        cities.add(city);
     }
 
 
